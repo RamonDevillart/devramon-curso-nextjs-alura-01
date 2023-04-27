eb66811b7372a1eaa0ce82399a29688119456eab
ramon <ramon.devillart@c2rio.travel>
2023-04-27 18:38:48 -0300

    push novo

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Footer from '../../components/patterns/footer';
+import Footer from '../../components/patterns/Footer';
 import Link from 'next/link';
 import { theme } from '../../theme/theme';
 import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
